dfc2dde64a22735db1f8b9112459a2408bd92921
Peconji <pecapeconji@gmail.com>
2023-08-18 00:26:45 +0200

    added content lang file

---
+++ lang/en/content.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'heroSection' => [
+        'heroTitle' => 'Inspiration for your next trip',
+        'heroText' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sunt debitis commodi accusamus quae id? Ut sapiente quia maiores iusto neque fuga, totam quaerat eum similique? Illum neque quas soluta.'
+    ],
+    'heroSection2' => [
+        'firstButton' => [
+            'text' => 'Znamenitosti',
+            'class' => 'button buttons btn-1',
+            'url' => '#',
+        ],
+        'secondButton' => [
+            'text' => 'Izdvajamo',
+            'class' => 'button buttons btn-2',
+            'url' => '#',
+        ],
+        'thirdButton' => [
+            'text' => 'Hoteli',
+            'class' => 'button buttons btn-3',
+            'url' => '#',
+        ],
+    ],
+    'afterHeroSection' => [
+        'afterHeroTitle' => 'Slider goes here',
+        'afterHeroText' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic \' Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias obcaecati nisi recusandae placeat sed minima ab totam maxime nam voluptas facilis fugit nobis, dolorem eum quasi ratione repellendus nesciunt at.',
+
+        'afterHeroSectionButton' => [
+            'text' => 'Explore Finnish nature',
+            'class' => 'button hero-button',
+            'url' => '#',
+        ],
+    ],
+    'afterHeroSection2' => [
+        'afterHeroTitle' => 'Vesti',
+        'afterHeroText' => 'Still searching for your dream destination? These articles might help.',
+
+        'afterHeroSectionButton2' => [
+            'text' => 'Read all articles',
+            'class' => 'button after-hero-button',
+            'url' => '#'
+        ],
+    ],
+    'beforeFooterSection' => [
+        'beforeFooterSectionTitle1' => 'Nine hot summer cities in Finland',
+        'beforeFooterSectionText1' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut soluta sapiente eius eaque! Veniam explicabo laboriosam ea consequatur nemo laudantium omnis architecto itaque adipisci. Laborum nobis voluptate consectetur non beatae.',
+        'beforeFooterSectionTitle2' => 'Expirience the Turku region',
+        'beforeFooterSectionText2' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut soluta sapiente eius eaque! Veniam explicabo laboriosam ea consequatur nemo laudantium omnis architecto itaque adipisci. Laborum nobis voluptate consectetur non beatae.',
+        'beforeFooterSectionTitle3' => 'Tampere - the sauna capital of the world',
+        'beforeFooterSectionText3' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut soluta sapiente eius eaque! Veniam explicabo laboriosam ea consequatur nemo laudantium omnis architecto itaque adipisci. Laborum nobis voluptate consectetur non beatae,',
+    ],
+];
