04419cb06777ed6c8dd80d68690e40a2f9b95713
Sofija <sofijatomasevic05@gmail.com>
2023-08-18 22:27:44 +0200

    Nav localisation buttons

--- resources/views/partials/_navigation.blade.php
+++ resources/views/partials/_navigation.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-xl navbar-dark bg-body-tertiary nav-section">
     <div class="container-fluid nav-box">
-        <img src="img/layout/logo.png" class="img-fluid nav-img">
+        <a class="navbar-brand" href="#"><img src="img/layout/logo.png" class="img-fluid nav-img"></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse nav-container" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav w-100">
                 @foreach( __('nav.list') as $list)
                     <li class="{{ $list['class'] }}">
                         <a href="{{ $list['button']['url'] }}" class="{{ $list['button']['class'] }}">
@@ -16,11 +16,6 @@
                     </li>
                 @endforeach
             </ul>
-        </div>
-
-        <div class="row">
-            <a href="#" class="icon icon-lang">EN</a>
-            <a href="#" class="icon-search"></a>
         </div>
     </div>
 </nav>
