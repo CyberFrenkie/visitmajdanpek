e5089ee6437abbff23793531b98cb49b9713261d
CyberFrenky <vitezkojampek@gmail.com>
2023-08-20 12:45:32 +0200

    Created first section

--- lang/en/home.php
+++ lang/en/home.php
@@ -1,5 +1,35 @@
 <?php
 
 return [
-    'welcome' => 'Welcome to Majdanpek',
+    'article' => [
+        [
+            'heading' => 'Inspiration for your next trip',
+
+            'paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Aspernatur alias veniam neque ratione beatae cupiditate fuga. 
+                            Voluptatum iste est, facere, quis iusto, 
+                            magnam non rem nostrum magni obcaecati quisquam sit.',   
+            
+            'btn' => [
+                [
+                    'img' => 'img/content/content.png',
+                    'text' => 'Trending',
+                    'link' => '/',
+                ],
+           
+                [
+                    'img' => 'img/content/content.png',
+                    'text' => 'Featured',
+                    'link' => '/',
+                ],
+           
+                [
+                    'img' => 'img/content/content.png',
+                    'text' => 'Hotels',
+                    'link' => '/',
+                ],
+            ]
+                
+        ]
+    ],
 ];
