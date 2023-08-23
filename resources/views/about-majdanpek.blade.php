e0751adf4e243adf119673dda557a3998c71634a
Sofija <sofijatomasevic05@gmail.com>
2023-08-23 20:19:59 +0200

    Feature about Majdanpek page

--- resources/views/about-majdanpek.blade.php
+++ resources/views/about-majdanpek.blade.php
@@ -10,7 +10,35 @@
   </div>
 
   <div class="main">
-    
+
+    <div class="container about-majdanpek-section">
+      <div class="row">
+        <div class="col-md-5">
+          <h3 class="title">{{ __('about-majdanpek.content1.title') }}</h3>
+          @foreach( __('about-majdanpek.content1.lead') as $lead )
+            <p class="lead">{{ $lead['text'] }}</p>
+          @endforeach
+        </div>
+        <div class="col-md-7">
+          <a href="#"><img src="img/content/content.png" class="img-fluid img"></a>
+        </div>
+      </div>  
+    </div>
+
+    <div class="container about-majdanpek-section">
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#"><img src="img/content/content.png" class="img-fluid img"></a>
+        </div>
+        <div class="col-md-5">
+          <h3 class="title">{{ __('about-majdanpek.content1.title') }}</h3>
+          @foreach( __('about-majdanpek.content1.lead') as $lead )
+            <p class="lead">{{ $lead['text'] }}</p>
+          @endforeach
+        </div>
+      </div>  
+    </div>
+
   </div>
 
   <div class="footer">
