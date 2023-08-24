cf983473afaeeeab0e1deef2c105b99f0b738976
Sofija <sofijatomasevic05@gmail.com>
2023-08-24 15:08:22 +0200

    About Majdanpek foreach & row-reversed

--- resources/views/about-majdanpek.blade.php
+++ resources/views/about-majdanpek.blade.php
@@ -11,33 +11,22 @@
 
   <div class="main">
 
-    <div class="container about-majdanpek-section">
-      <div class="row">
-        <div class="col-md-5">
-          <h3 class="title">{{ __('about-majdanpek.content1.title') }}</h3>
-          @foreach( __('about-majdanpek.content1.lead') as $lead )
-            <p class="lead">{{ $lead['text'] }}</p>
-          @endforeach
-        </div>
-        <div class="col-md-7">
-          <a href="#"><img src="img/content/content.png" class="img-fluid img"></a>
-        </div>
-      </div>  
-    </div>
-
-    <div class="container about-majdanpek-section">
-      <div class="row">
-        <div class="col-md-7">
-          <a href="#"><img src="img/content/content.png" class="img-fluid img"></a>
-        </div>
-        <div class="col-md-5">
-          <h3 class="title">{{ __('about-majdanpek.content1.title') }}</h3>
-          @foreach( __('about-majdanpek.content1.lead') as $lead )
-            <p class="lead">{{ $lead['text'] }}</p>
-          @endforeach
-        </div>
-      </div>  
-    </div>
+    @foreach( __('about-majdanpek.content') as $content )
+      <div class="container about-majdanpek-section">
+        <div class="{{ $content['class'] }}">
+            <div class="col-md-5">
+              <h3 class="title">{{ $content['title'] }}</h3>
+              @foreach( $content['lead'] as $lead )
+                <p class="lead">{{ $lead['text'] }}</p>
+              @endforeach
+            </div>
+            
+            <div class="col-md-7">
+              <a href="{{ $content['img']['link'] }}"><img src="{{ $content['img']['src'] }}" class="img-fluid img"></a>
+            </div>
+        </div>  
+      </div>
+    @endforeach
 
   </div>
 
