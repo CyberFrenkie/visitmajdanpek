a1f43d75e9c38a69fe14f362653bf96bd2efd4d7
Sofija <sofijatomasevic05@gmail.com>
2023-08-24 17:57:48 +0200

    Sights page additions

--- resources/views/sights.blade.php
+++ resources/views/sights.blade.php
@@ -13,20 +13,18 @@
     
     <div class="container-xl sights-content">
       @foreach( __('sights.sights') as $sights )
-
-        <div class="content-wrap col-12 row mx-1 justify-content-between sights-text-con">
-          <h1 class="col-md-4 col-sm-12 news-heading px-0">{{ $sights['head'] }}</h1>
-          <p class="col-md-6 col-sm-12 news-text px-0">{{ $sights['par'] }}</p>
-          <a href="{{ $sights['btn']['link'] }}" class="{{ $sights['btn']['class'] }}">{{ $sights['btn']['text'] }}</a>
+        <div class="sights-text-con">
+          <h1 class="news-heading px-0">{{ $sights['head'] }}</h1>
+          <hr class="seperator col-md-4 col-sm-12 marg">
         </div>
 
         <div class="card-wrap row mx-auto my-5 flex-wrap">
           @foreach ( ($sights['content']) as $content )
             <div class="card col-md-4 col-sm-12 sights-card">
               <a href="{{ $content['link'] }}" class="card-link link" >
-                <img src="{{ $content['img'] }}" alt="" class="card-img">
+                <img src="{{ $content['img'] }}" alt="" class="card-img img">
                 <div class="card-content">
-                  <h1 class="card-info-header m-2">{{ $content['head'] }}</h1>
+                  <h1 class="card-info-header sights-card-header m-2">{{ $content['head'] }}</h1>
                   <p class="card-info-text m-2">{{ $content['par'] }}</p>
                 </div>
               </a>                        
