0e6529f08084be2456dcdd980f9fb05c1b7f4827
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-08-25 16:31:13 +0200

    fixed contact us page and finished it.

--- lang/en/contact.php
+++ lang/en/contact.php
@@ -10,21 +10,33 @@ return [
         
         'firstName' => [
             'label' => 'First Name',
-            'placeholder' => 'Enter your first name'
+            'placeholder' => 'Enter your first name',
+            'class' => 'form-control',
+            'name' => 'firstName',
         ],
         'lastName' => [
             'label' => 'Last Name',
-            'placeholder' => 'Enter your last name'
+            'placeholder' => 'Enter your last name',
+            'class' => 'form-control',
+            'name' => 'lastName',
         ],
         'email' => [
             'label' => 'Email Address',
-            'placeholder' => 'Enter your email'
+            'placeholder' => 'Enter your email',
+            'class' => 'form-control',
+            'name' => 'email',
         ],
         'inquiry' => [
             'label' => 'Tell us more',
-            'placeholder' => 'Tell us more'
+            'placeholder' => 'Tell us more',
+            'class' => 'form-control',
+            'name' => 'message',
         ]
     ],
-    'send' => 'Send'
+    'send' => 'Send',
+    'btn-class' => 'send-button',
+    'contact-us-header-text' => 'Contact us',
+    'form-class' => 'form-styling',
+    'contact-us-header-class' => 'contact-header-title',
     
 ];
