5cf5ff57964c0e61b2b07de5cfcf4598477ebc7a
CyberFrenky <vitezkojampek@gmail.com>
2023-09-05 17:44:56 +0200

    Deleteing empty spaces for better code reading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,19 @@
 @extends('layouts/layout')
-
 @section('title', 'VisitMajdanpek - Pocetna')
-
 @section('wrapper')
 
-<div class="home-wrapper container-fluid">
-    <div class="header">
-        @include('components.header')
-    </div>
+    <div class="home-wrapper container-fluid">
+        <div class="header">
+            @include('components.header')
+        </div>
 
-    <div class="main">
-        @include('components.main')
-    </div>
+        <div class="main">
+            @include('components.main')
+        </div>
 
-    <div class="footer">
-        @include('components.footer')
+        <div class="footer">
+            @include('components.footer')
+        </div>
     </div>
-</div>
 
 @endsection
