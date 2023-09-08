0445a6de89124c10ba563e2077ae9e8b7e1abe0e
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-09-08 15:04:04 +0200

    added custom sooter on single sight page

---
+++ lang/en/custom-footer.php
@@ -0,0 +1,130 @@
+<?php
+return[
+    'list1'=> [
+        [
+        'list1Values'=> [
+            [
+                    'text'=>'Location',
+                    'url'=>'https://goo.gl/maps/sgbbEVpLpRhyEr6SA',
+                    'icon'=>'fa-solid fa-location-dot me-2',
+            ],
+            [
+                    'text'=>'7:30-14:30',
+                    'url'=>'',
+                    'icon'=>'fa-regular fa-clock me-2', 
+            ],
+        ],
+        ],
+    ],
+        'list2'=> [
+        [
+        'list2Values'=> [
+            [
+                    'text'=>'Car Route',
+                    'url'=>'https://www.google.com/maps/dir/?api=1&destination=Rajkova+Pecina&travelmode=driving&dir_action=navigate',
+                    'icon'=>'fa-solid fa-car me-2',
+            ],
+            [
+                    'text'=>'000-000-0000',
+                    'url'=>'',
+                    'icon'=>'fa-solid fa-phone-volume me-2',
+            ],
+        ],
+        ],
+    ],
+            'list3'=> [
+        [
+        'list3Values'=> [
+            [
+                    'text'=>'Walk Route',
+                    'url'=>'https://www.google.com/maps/dir/?api=1&destination=Rajkova+Pecina&travelmode=walking&dir_action=navigate',
+                    'icon'=>'fa-solid fa-person-walking me-2',
+            ],
+            [
+                    'text'=>'exampleemail2@gmail.com',
+                    'url'=>'',
+                    'icon'=>'fa-regular fa-envelope me-2',
+            ],
+        ],
+        ],
+    ],
+
+     'social' => [
+                [
+                'socialValues' => [
+
+                        [
+                        'icon' => 'fa-brands fa-square-facebook',
+                        'url' => '/',
+                        'class' => 'me-3',
+
+                        ],
+                        [
+                        'icon' => 'fa-brands fa-instagram',
+                        'url' => '/',
+                        'class' => 'me-3',
+
+                        ],
+                        [
+                        'icon' => 'fa-brands fa-twitter',
+                        'url' => '/',
+                        'class' => 'me-3',
+
+                        ],
+                        [
+                        'icon' => 'fa-solid fa-hashtag',
+                        'url' => '/',
+                        'class' => 'me-3',
+                        ],
+
+                        ]
+                ]
+        
+        ],
+        'slider' => [
+            [
+                'sliderValues' => [
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                    [
+                    'url' => '/',
+                    'src' => '/img/content/content.png',
+                    'text' => 'Placeholder',
+                    ],
+                ],
+            ]
+        ],
+];
