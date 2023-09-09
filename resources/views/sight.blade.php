d520d5a2b231e1a117a03c4cf9d1f0775ae0bc20
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-09-09 16:39:59 +0200

    added different slider and fixed alignment issues

--- resources/views/sight.blade.php
+++ resources/views/sight.blade.php
@@ -29,13 +29,13 @@
       <div class="container">
         <div class="row">
           @foreach( $sight as $index => $content )
-          <div class="d-flex align-items-center justify-content-evenly {{ $index % 2 == 0 ? 'flex-row-reverse' : 'flex-row' }}">
-            <div class="col-md-7">
+          <div class="d-flex  align-items-center justify-content-evenly flex-column-reverse {{ $index % 2 == 0 ? 'flex-lg-row-reverse' : 'flex-lg-row' }}">
+            <div class="col-lg-7 col-md-12 text-lg-start text-center">
               <h3 class="title">{{ $content['head'] }}</h3>
                 <p class="lead">{{ $content['par'] }}</p>
             </div>
             
-            <div class="col-md-4 me-2">
+            <div class="col-lg-4 col-md-12 me-2 mb-2 mb-sm-2">
               <a href="/"><img src="{{asset($item->img)}}" class="card-img img"></a>
             </div>
           </div>
