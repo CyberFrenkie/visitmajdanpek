5cfb1a4a74a835905832a6c6e325efc29a6ca89f
Sofija <sofijatomasevic05@gmail.com>
2023-09-10 14:34:57 +0200

    Added custom hero to sight page

--- resources/js/scroll.js
+++ resources/js/scroll.js
@@ -1,34 +1,47 @@
+// Custom hero & Top button
+
 document.addEventListener('DOMContentLoaded', function () {
-const mybutton = document.getElementById("myBtn");
+  const mybutton = document.getElementById("myBtn");
+  const customHero = document.getElementById("custom-hero");
+  const middleElement = document.getElementById("middle");
 
-mybutton.addEventListener("click", function () {
-    topFunction();
-});
+  if (mybutton) {
+    mybutton.addEventListener("click", topFunction);
+  }
 
-
-function topFunction() {
+  function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+  }
 
+  function toggleButtonVisibility() {
+    const scrollY = window.scrollY;
+    mybutton.style.display = scrollY > 20 ? "block" : "none";
+  }
 
-window.onscroll = function () {
-    scrollFunction();
-};
+  function adjustHeroHeight() {
+    const scrollY = window.scrollY;
+    customHero.style.height = scrollY > 20 ? '30vh' : '100vh';
+  }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-    } else {
-    mybutton.style.display = "none";
-    }
-}
+  function adjustMiddleMargin() {
+    const scrollY = window.scrollY;
+    middleElement.style.marginTop = scrollY > 20 ? '35vh' : '100vh';
+  }
+
+  window.addEventListener('scroll', () => {
+    toggleButtonVisibility();
+    adjustHeroHeight();
+    adjustMiddleMargin();
+  });
 });
 
+
+// Scroller
 
 const scrollContainer = document.getElementById("scroller");
 
 scrollContainer.addEventListener("wheel", (evt) => {
-evt.preventDefault();
-scrollContainer.scrollLeft += evt.deltaY-30;
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY-30;
 });
--- resources/views/sight.blade.php
+++ resources/views/sight.blade.php
@@ -6,42 +6,38 @@
     
     {{-- Header --}}
     <div class="header">
-        @include('partials/_navigation')
+      @include('partials/_navigation')
+
+      <div id="custom-hero">
+        @foreach ($sight as $item)
+          <img src="{{asset($item->img)}}" alt="" class="img">
+
+          <div class="container">       
+            <h3>{{ $item->head }}</h1>                    
+            <p>{{ $item->par }}</p>  
+          </div>
+        @endforeach
+      </div>
     </div>
 
     {{-- Main section --}}
-    <div class="main"> 
-      <div class="container-xl sights-content"> 
-        <div class="row mx-auto my-5 flex-row">
-          @foreach ($sight as $item)    
-            <div class=" col-md-12 col-sm-12">                  
-              <img src="{{asset($item->img)}}" alt="" class="card-img img">  
-              <div class="card-content">  
-                <h1 class="card-info-header sights-card-header m-2">{{ $item->head }}</h1>                    
-                <p class="card-info-text m-2">{{ $item->par }}</p>  
-              </div> 
-            </div>
-          @endforeach  
-        </div>           
-      </div>   
-    </div>
-    <div class="middle">
+    <div id="middle">
       <div class="container">
         <div class="row">
           @foreach( $sight as $index => $content )
-          <div class="d-flex  align-items-center justify-content-evenly flex-column-reverse {{ $index % 2 == 0 ? 'flex-lg-row-reverse' : 'flex-lg-row' }}">
-            <div class="col-lg-7 col-md-12 text-lg-start text-center">
-              <h3 class="title">{{ $content['head'] }}</h3>
+            <div class="d-flex  align-items-center justify-content-evenly flex-column-reverse {{ $index % 2 == 0 ? 'flex-lg-row-reverse' : 'flex-lg-row' }}">
+              <div class="col-lg-7 col-md-12 text-lg-start text-center">
+                <h3 class="title">{{ $content['head'] }}</h3>
                 <p class="lead">{{ $content['par'] }}</p>
+              </div>
+              
+              <div class="col-lg-4 col-md-12 me-2 mb-2 mb-sm-2">
+                <a href="/"><img src="{{asset($item->img)}}" class="card-img img"></a>
+              </div>
             </div>
-            
-            <div class="col-lg-4 col-md-12 me-2 mb-2 mb-sm-2">
-              <a href="/"><img src="{{asset($item->img)}}" class="card-img img"></a>
             </div>
-          </div>
+          @endforeach
         </div>
-      @endforeach
-      </div>
       </div>
     </div>
 
