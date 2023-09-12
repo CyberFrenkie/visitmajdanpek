494c0ae7bcc1afd8d15d94ea0bf8f2c5efbf54f9
CyberFrenky <vitezkojampek@gmail.com>
2023-09-12 16:56:11 +0200

    Added function to seed database from lang file

---
+++ lang/en/news.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+  'news' => [
+      'head' => 'Znamenitosti',
+
+      'content' => [
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],
+
+        [
+          'link' => '/',
+          'img' => 'img/content/news/news-default.jpg',     
+          'head' => 'News 1',
+          'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
+       
+        ],       
+    ],
+  ],
+
+];
