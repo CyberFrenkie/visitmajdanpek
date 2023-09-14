731e8a476bf79252271d99bd6e34dcf791fc9714
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-09-14 21:28:02 +0200

    fixed scroll appearing

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,14 +3,14 @@
     <div class="container">
         <hr class="col-md-4 col-sm-12 px-0">
         <div class="row">
-            <div class="col-lg-3 my-4">
+            <div class="col-lg-3 my-lg-4">
             <a href=" {{ trans('footer.far-left-link') }} " class=" {{ trans('footer.default-class') }} "> 
                 <p class="{{ trans('footer.upper-text-class') }} {{ trans('footer.default-class') }}"> {{ trans('footer.upper-text') }} </p>
                 <p class="{{ trans('footer.bottom-text-class') }} {{ trans('footer.default-class') }}"> {{ trans('footer.bottom-text') }} </p>
             </a> 
                 <p class="{{ trans('footer.copyright-class') }} {{ trans('footer.default-class') }}"> {{ trans('footer.copyright') }}</p>
             </div>
-            <div class="row col-lg-6  my-4 justify-center cal-pl">
+            <div class="row col-lg-6  my-lg-4 my-3 justify-center cal-pl">
             <div class="col-lg-4 col-md-4 col-sm-12">
                 <h3 class="{{ trans('footer.list-title-class') }}">{{ trans('footer.left-title') }}</h3>
                 <ul class="{{ trans('footer.default-class') }}"> 
@@ -36,7 +36,7 @@
                 </ul>
             </div>
             </div>
-            <div class="col-lg-3  my-4">
+            <div class="col-lg-3  my-lg-4">
                     <h2 class="{{ trans('footer.social-class') }}">{{ trans('footer.social-title') }}</h2>
                     <ul class="d-flex align-items-center justify-center {{ trans('footer.default-class') }}">
                     @foreach(trans('footer.social') as $social)
