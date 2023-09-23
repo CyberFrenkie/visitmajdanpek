fa2c783f9c57195ce810136382ba2896cba04b2f
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-09-23 22:38:39 +0200

    added our email to contact page

--- lang/en/contact.php
+++ lang/en/contact.php
@@ -25,5 +25,5 @@ return [
     'form-class' => 'form-styling',
     'contact-us-header-class' => 'contact-header-title',
     'contact-us-text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
-    
+    'contact-us-email'=>'Takodje nas mozete kontaktirati putem:<span class="d-block contact-us-email"><i class="fa-regular fa-envelope me-1"></i><a href="mailto:visitmajdanpek@gmail.com" class="default">visitmajdanpek@gmail.com</a></span>'
 ];
