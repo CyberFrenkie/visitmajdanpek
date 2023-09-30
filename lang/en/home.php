55b636545bd5145c7b574ba0ba6ba9639cbe7b81
Sofija <sofijatomasevic05@gmail.com>
2023-09-30 16:52:55 +0200

    Home slide & news sections
    Added heading & seperator to news section.
    Styled read more button.
    
    Added overlay,links & images to slide section.

--- lang/en/home.php
+++ lang/en/home.php
@@ -5,11 +5,7 @@ return [
     'article' => [
         [
             'heading' => 'Inspiracija za vaše sledeće putovanje',
-
-            'paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur alias veniam neque ratione beatae cupiditate fuga. 
-                            Voluptatum iste est, facere, quis iusto, 
-                            magnam non rem nostrum magni obcaecati quisquam sit.',  
+            'paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur alias veniam neque ratione beatae cupiditate fuga. Voluptatum iste est, facere, quis iusto, magnam non rem nostrum magni obcaecati quisquam sit.',
             
             'btn' => [
                 [
@@ -27,126 +23,66 @@ return [
                     'text' => 'Prevoz',
                     'link' => '/transportation',
                 ],
-            ]
-        ]
+            ],
+        ],
     ],
-    
+
     'hero' => [
         'carousel' => [
             'indicators' => [
                 [
-                    'class' => 'active', 
-                    'label' => 'Slide 1'
-                ],
-                
-                [
-                    'class' => '', 
-                    'label' => 'Slide 2'
+                    'class' => 'active',
+                    'label' => 'Slide 1',
                 ],
                 [
-                    'class' => '', 
-                    'label' => 'Slide 3'
+                    'class' => '',
+                    'label' => 'Slide 2',
+                ],
+                [
+                    'class' => '',
+                    'label' => 'Slide 3',
                 ],
             ],
 
-            'controls' => [    
+            'controls' => [
                 'prev' => [
-    
                     'class' => 'carousel-control-prev',
                     'icon' => 'carousel-control-prev-icon',
                     'label' => 'Previous',
-                    'slide' => 'prev', 
-    
+                    'slide' => 'prev',
                 ],
 
                 'next' => [
-    
                     'class' => 'carousel-control-next',
                     'icon' => 'carousel-control-next-icon',
                     'label' => 'Next',
-                    'slide' => 'next', 
-    
+                    'slide' => 'next',
                 ],
-            ],
-    
-            'image' => [
-    
-                'src' => 'img/hero/default-img.png',
-                'class' => 'd-block w-100 h-100',
-                'alt' => 'Carousel Image',
-    
             ],
 
             'slides' => [
                 [
+                    'img' => 'img/content/majdanpek/mpek-nocu.jpg',
                     'title' => 'Preporučujemo',
                     'text' => 'Prepoznajte destinaciju iz snova!',
                     'button_text' => 'Pročitaj više',
-                    'button_class' => 'btn readMore',
+                    'button_link' => '/sights/dm-sights',
                 ],
                 [
+                    'img' => 'img/content/majdanpek/majdanpek.jpg',
                     'title' => 'Popularno',
                     'text' => 'Prepoznajte destinaciju iz snova!',
                     'button_text' => 'Pročitaj više',
-                    'button_class' => 'btn readMore',
+                    'button_link' => '/sights/mpek',
                 ],
                 [
+                    'img' => 'img/content/majdanpek/mpek2.jpg',
                     'title' => 'Manje poznato',
                     'text' => 'Prepoznajte destinaciju iz snova!',
                     'button_text' => 'Pročitaj više',
-                    'button_class' => 'btn readMore',
+                    'button_link' => '/about-majdanpek',
                 ],
             ],
         ],
     ],
-   
-    'news' => [
-        [
-            'heading' => 'Novosti',
-            'paragraph' => 'Da li ste i dalje u potrazi za idealnom destinacijom?',
-    
-            'btn' => [
-                [
-                    'text' => 'Pročitaj sve članke',
-                    'link' => '/',
-                ],
-            ],   
-    
-            'content' => [
-                [
-                    'img' => 'img/content/content.png',        
-                    'head' => 'header',
-        
-                    'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur alias veniam neque ratione beatae cupiditate fuga. 
-                            Voluptatum iste est, facere, quis iusto, 
-                            magnam non rem nostrum magni obcaecati quisquam sit.',
-
-                    'link' => '/',
-                ],                
-                [
-                    'img' => 'img/content/content.png',        
-                    'head' => 'header',
-        
-                    'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur alias veniam neque ratione beatae cupiditate fuga. 
-                            Voluptatum iste est, facere, quis iusto, 
-                            magnam non rem nostrum magni obcaecati quisquam sit.',
-
-                    'link' => '/',
-                ],                
-                [
-                    'img' => 'img/content/content.png',        
-                    'head' => 'header',
-        
-                    'par' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur alias veniam neque ratione beatae cupiditate fuga. 
-                            Voluptatum iste est, facere, quis iusto, 
-                            magnam non rem nostrum magni obcaecati quisquam sit.',
-
-                    'link' => '/',
-                ],
-            ],
-        ]
-    ],  
 ];
