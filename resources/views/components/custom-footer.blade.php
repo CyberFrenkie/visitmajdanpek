56bad5b83853427e7625a73b55732efcce74e445
mlekoodbika <krajnicanbusinessemail@gmail.com>
2023-10-13 22:17:00 +0200

    fixed fullscreen image and added ids to maps

--- resources/views/components/custom-footer.blade.php
+++ resources/views/components/custom-footer.blade.php
@@ -2,7 +2,7 @@
     <hr class="seperator col-md-4 col-sm-12">
     <div class="container">
         <div class="row ps-3">
-            <div class="col-lg-12 d-flex  p-0 flex-d-column">
+            <div class="col-lg-12 d-flex align-left p-0 flex-d-column">
             <ul class=" default nav-tabs gap-align mb-2" role="tablist">
             @foreach(trans('custom-footer.list1') as $list)
                 @foreach(($list['list1Values']) as $listValue)
@@ -13,11 +13,11 @@
             @php
                 $currentPageId=request()->segment(2);
             @endphp    
-            <ul class="gap-align default">
+            <ul class="gap-align no-flex default">
                 @foreach( trans('custom-footer.list2.content') as $list)
                     @foreach( ($list['list2Values']) as $listValue)
                         @if(   $list['id'] == $currentPageId)
-                            <li>
+                            <li class="mbm">
                                 <p class="{{ trans('footer.default-class') }} custom-footer-font-size fw-extra-medium">
                                     <i class="{{ $listValue['icon'] }}"></i> {{ $listValue['inner-text'] }}
                                 </p>
@@ -29,39 +29,52 @@
             </div>
         </div>
         <div class="tab-content my-5">
-            <div class="tab-pane fade-transition fade my-5" id="location" role="tabpanel" tabindex="0">
-                <p class="text-center custom-footer-font-size">{{ trans('custom-footer.locations') }}</p>
-                <div class="container d-flex justify-content-center pb-2">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5224.395476590407!2d21.95219135969145!3d44.440917724553586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47516f327533d39d%3A0xa001196a77495ff9!2z0KDQsNGY0LrQvtCy0LAg0L_QtdGb0LjQvdCw!5e0!3m2!1ssr!2srs!4v1695993075352!5m2!1ssr!2srs" width="1024" height="600" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-                <p class="text-center custom-footer-font-size">{!! trans('custom-footer.Informations') !!}</p>
-            </div>
-            <div class="tab-pane fade-transition fade" id="car-route" role="tabpanel" tabindex="0">
-                <div class="d-flex flex-d-column justify-content-evenly">
-                    <div class="me-3 mr-0">
-                        <p class="text-center custom-footer-font-size">{{ trans('custom-footer.BG-pravac') }}</p>
-                        <div class="d-flex justify-content-center align-items-center pb-2">
-                            <div class="google-map">
-                                <div id="google-maps-display" class="google-map-overlay">
-                                    <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/directions?origin=Belgrade,+Serbia&destination=Rajko's+Cave,+Капетанска,+Majdanpek,+Serbia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
-                                </div>
+            @foreach(trans('custom-footer.maps.content') as $mapContent)
+                @foreach(($mapContent['location']) as $location)
+                    @if($mapContent['id'] == $currentPageId)
+                        <div class="tab-pane fade-transition fade my-5" id="location" role="tabpanel" tabindex="0">
+                            <p class="text-center custom-footer-font-size">{{ trans('custom-footer.locations') }}</p>
+                            <div class="container d-flex justify-content-center pb-2">
+                                <iframe src="{{ $location['src'] }}" width="1024" height="600" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                            <p class="text-center custom-footer-font-size"><a href="{{ $location['src'] }}" target="_blank" class="default">{{ trans('custom-footer.information') }}<i class="move-arrow fa-solid fa-arrow-right ms-2"></i></a></p>
+                        </div>
+                    @endif
+                @endforeach
+            @endforeach
+            @foreach(trans('custom-footer.maps.content') as $mapContent)
+                @foreach(($mapContent['car-route']) as $route)
+                    @if($mapContent['id'] == $currentPageId)
+                        <div class="tab-pane fade-transition fade" id="car-route" role="tabpanel" tabindex="0">
+                            <div class="d-flex flex-d-column justify-content-evenly">
+                                <div class="me-3 mr-0">
+                                    <p class="text-center custom-footer-font-size">{{ trans('custom-footer.BG-pravac') }}</p>
+                                    <div class="d-flex justify-content-center align-items-center pb-2">
+                                        <div class="google-map">
+                                            <div id="google-maps-display" class="google-map-overlay">
+                                                <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="{{ $route['src1'] }}"></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <p class="text-center custom-footer-font-size"><a href="{{ $route['src1'] }}" target="_blank" class="default">Za vise informacija klikni OVDE<i class="move-arrow fa-solid fa-arrow-right ms-2"></i></a></p>
+                            </div>
+                            <div>
+                                <p class="text-center custom-footer-font-size">{{ trans('custom-footer.NG-pravac') }}</p>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <div class="google-map">
+                                            <div id="google-maps-display" class="google-map-overlay">
+                                                <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="{{ $route['src2'] }}"></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <p class="text-center custom-footer-font-size"><a href="{{ $route['src2'] }}" target="_blank" class="default">Za vise informacija klikni OVDE<i class="move-arrow fa-solid fa-arrow-right ms-2"></i></a></p>
                             </div>
                         </div>
-                        <p class="text-center custom-footer-font-size">{!! trans('custom-footer.Informations') !!}</p>
-                    </div>
-                    <div>
-                        <p class="text-center custom-footer-font-size">{{ trans('custom-footer.NG-pravac') }}</p>
-                        <div class="d-flex justify-content-center align-items-center">
-                            <div class="google-map">
-                                <div id="google-maps-display" class="google-map-overlay">
-                                    <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/directions?origin=Negotin,+Serbia&destination=Rajko's+Cave,+Капетанска,+Majdanpek,+Serbia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
-                                </div>
-                            </div>
                         </div>
-                        <p class="text-center custom-footer-font-size">{!! trans('custom-footer.Informations') !!}</p>
-                    </div>
-                </div>
-            </div> 
+                        @endif
+                @endforeach
+            @endforeach
+            {{-- 
             <div class="tab-pane fade-transition fade" id="walk-route" role="tabpanel" tabindex="0">
                <div class="d-flex flex-d-column justify-content-evenly">
                     <div class="me-3 mr-0">
@@ -87,8 +100,11 @@
                         <p class="text-center custom-footer-font-size">{!! trans('custom-footer.Informations') !!}</p>
                     </div>
                 </div>
+            </div>
+            --}}
         </div>
-    <div id="carouselExampleControls" class="carousel slide pl-15 pb-4" data-bs-ride="carousel">
+    </div>    
+    <div id="carouselExampleControls" class="carousel slide pb-4" data-bs-ride="carousel">
         <div class="carousel-inner mb-3">
             @foreach( trans('custom-footer.slider.content') as $slider)
                @foreach( $slider['sliderValues'] as $sliderValue)
@@ -115,5 +131,8 @@
             </button>
         </div>  
     </div>
-    <div id="fullscreen"></div>
+    <div id="fullscreen">
+        <img src="" alt="" class="fullscreen-image">
+        <button id="closeButton" class="fullscreen-button">X</button>
+    </div>
 </footer>
