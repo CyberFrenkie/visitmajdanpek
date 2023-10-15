bd1fe3561b644baf1eb844cab748b7d92d7b5382
CyberFrenky <vitezkojampek@gmail.com>
2023-10-15 13:42:53 +0200

    fixed items alignement

--- resources/views/components/news.blade.php
+++ resources/views/components/news.blade.php
@@ -19,13 +19,13 @@
         <h1 class="col-md-6 col-sm-12 news-heading px-0">{{__('news.news.head')}}</h1>
       </div>
 
-      <div class="news-card-wrap flex-wrap">
+      <div class="news-card-wrap">
         @foreach ( $news as $index => $content )
-        <div class="news-card col-md-12">
-          <div class="news-card-link flex-wrap {{ $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse' }}">
+        <div class="news-card col-md-4 col-sm-12">
+          <div class="news-card-link">
             <a href="/new/{{ $content['id'] }}" class="link">
-              <img src="{{ $content->img }}" alt="" class="news-card-img col-md-4 col-sm-12">
-              <div class="news-card-content col-md-8 col-sm-12">
+              <img src="{{ $content->img }}" alt="" class="news-card-img ">
+              <div class="news-card-content">
                 <h1 class="card-info-header m-2">{{ $content->head }}</h1>
                 <p class="card-info-text m-2">{{ $content->par }}</p>
                 <a href="/new/{{ $content['id'] }}" class="cta readMore d-block">{{ __('news.news.btn_text') }} <i class="{{ trans('custom-footer.arrow-icon') }}"></i></a>
